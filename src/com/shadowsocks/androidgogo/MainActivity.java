419e1e1e659ee0bae5d0a2266617c25e219732ec
chiluobo <huyang191@163.com>
2016-12-31 01:39:14 +0800

    1111111

--- src/com/shadowsocks/androidgogo/MainActivity.java
+++ src/com/shadowsocks/androidgogo/MainActivity.java
@@ -2,27 +2,60 @@ package com.shadowsocks.androidgogo;
 
 import com.shadow.bean.ShadowTotalBean;
 import com.shadow.util.HelloWorld;
+import com.shadowsocks.util.ConvertUtil;
 
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	private ShadowTotalBean mShadowTotalBean;
+	LinearLayout linlaHeaderProgress;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		linlaHeaderProgress = (LinearLayout) findViewById(R.id.linlaHeaderProgress);
+		findViewById(R.id.button).setOnClickListener(this);
 	}
 
-	class MyAsyncTask extends AsyncTask<Void, Void, Void> {
+	class MyAsyncTask extends AsyncTask<Void, Void, String> {
+		@Override
+		protected void onPreExecute() {
+			// SHOW THE SPINNER WHILE LOADING FEEDS
+			linlaHeaderProgress.setVisibility(View.VISIBLE);
+		}
 
 		@Override
-		protected Void doInBackground(Void... params) {
+		protected String doInBackground(Void... params) {
 			mShadowTotalBean = HelloWorld.getInfo();
-			return null;
+			String info = ConvertUtil.convertBean(mShadowTotalBean);
+			return info;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			ConvertUtil.copy(result, MainActivity.this);
+			linlaHeaderProgress.setVisibility(View.GONE);
+		}
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button:
+			new MyAsyncTask().execute();
+			break;
+
+		default:
+			break;
 		}
 
 	}
--- src/com/shadowsocks/util/ConvertUtil.java
+++ src/com/shadowsocks/util/ConvertUtil.java
@@ -2,6 +2,11 @@ package com.shadowsocks.util;
 
 import java.util.ArrayList;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.util.Base64;
+
 import com.shadow.bean.ShadowInfoBean;
 import com.shadow.bean.ShadowTotalBean;
 
@@ -12,12 +17,39 @@ public class ConvertUtil {
 				|| bean.getConfigs().isEmpty()) {
 			return "";
 		}
-		StringBuffer buffer=new StringBuffer();
+		StringBuffer buffer = new StringBuffer();
+		StringBuffer totalBuffer = new StringBuffer();
 		ArrayList<ShadowInfoBean> infoBeans = bean.getConfigs();
-		for(int i=0;i<infoBeans.size();i++){
-			ShadowInfoBean infoBean=infoBeans.get(i);
-			buffer.append(infoBean.getMethod()).append(":").append(infoBean.getPassword()).append("@").append(infoBean.getServer()).append(":").append(infoBean.getServer_port());
+		for (int i = 0; i < infoBeans.size(); i++) {
+			ShadowInfoBean infoBean = infoBeans.get(i);
+			if (infoBean != null) {
+				totalBuffer.append("\n" + "ss://");
+				buffer.append(infoBean.getMethod()).append(":")
+						.append(infoBean.getPassword()).append("@")
+						.append(infoBean.getServer()).append(":")
+						.append(infoBean.getServer_port());
+				totalBuffer.append(base64(buffer));
+			}
 		}
-		return null;
+		return totalBuffer.toString();
+	}
+
+	public static String base64(StringBuffer buffer) {
+		String baseInfo = Base64.encodeToString(buffer.toString().getBytes(),
+				Base64.DEFAULT);
+		buffer.setLength(0);
+		return baseInfo;
+	}
+
+	/**
+	 * 实现文本复制功能
+	 * 
+	 * @param content
+	 */
+	public static void copy(String content, Context context) {
+		// 得到剪贴板管理器
+		ClipboardManager cmb = (ClipboardManager) context
+				.getSystemService(Context.CLIPBOARD_SERVICE);
+		cmb.setPrimaryClip(ClipData.newPlainText(null, content));
 	}
 }
